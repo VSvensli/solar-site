ab22b22b77ae99cb6da9f9e8bff8a02c321d387d
VSvensli <mrsvensli@gmail.com>
2025-02-23 22:55:53 +0100

    Update cell selector style

--- src/components/CellSelector.vue
+++ src/components/CellSelector.vue
@@ -5,7 +5,6 @@ import { onMounted } from "vue";
 import { storeToRefs } from "pinia";
 
 const projectStore = useProjectStore();
-const userStore = useUserStore();
 const { panelArray } = storeToRefs(projectStore);
 
 const props = defineProps<{ projectId: string }>();
@@ -17,26 +16,39 @@ onMounted(() => {
 
 <template>
   <div>
+    {{ panelArray }}
+  </div>
+  <div>
     CellSelector
     <div v-if="panelArray" class="flex flex-wrap gap-5">
       <div v-for="panel in panelArray" :key="panel.id">
         <div
-          class="grid gap-1 p-2 bg-slate-500 shadow-sm rounded-md"
+          class="grid gap-1 p-2 bg-slate-200 shadow-md rounded-md"
           :style="{
             gridTemplateColumns: 'repeat(' + panel.cellColumns + ', 50px)',
             gridTemplateRows: 'repeat(' + panel.cellRows + ', 50px)',
           }"
         >
-          <div
-            v-for="cell in panel.cells"
-            class="bg-blue-200 rounded-xl border-5 border-blue-100 border-opacity-50"
-            :key="cell.id"
-          >
+          <div v-for="cell in panel.cells" class="bg-blue-500 module" :key="cell.id">
             {{ cell.id }}
           </div>
         </div>
       </div>
     </div>
   </div>
-  <div class="w-100 h-100 bg-blue-100">hello</div>
 </template>
+
+<style scoped>
+.module {
+  clip-path: polygon(
+    0% 5px,
+    /* top left */ 5px 0%,
+    /* top left */ calc(100% - 5px) 0%,
+    /* top right */ 100% 5px,
+    /* top right */ 100% calc(100% - 5px),
+    /* bottom right */ calc(100% - 5px) 100%,
+    /* bottom right */ 5px 100%,
+    /* bottom left */ 0 calc(100% - 5px) /* bottom left */
+  );
+}
+</style>
