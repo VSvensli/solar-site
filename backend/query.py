71c41de12b111dffca058aba1d97e785ff23c0cf
VSvensli <mrsvensli@gmail.com>
2025-04-04 23:15:20 +0200

    Create a simple ORM for quarying

--- backend/query.py
+++ backend/query.py
@@ -1,5 +1,8 @@
 import sqlite3
-from backend.schemas import (
+import datetime
+from dataclasses import fields
+from collections import namedtuple
+from schemas import (
     DBProject,
     DBPanel,
     DBCell,
@@ -7,119 +10,122 @@ from backend.schemas import (
     DBPowerDataPoint,
     DBUser,
     DBUserProject,
+    DBTypes,
 )
 
 DB_NAME = "solar.db"
+IntervalSelection = namedtuple("IntervalSelection", ["column", "start_date", "end_date"])
+
+
+class SimpleORM:
+    def __init__(self, db_name: str = DB_NAME):
+        self.db_name = db_name
+
+    @classmethod
+    def quary(cls, model_class: DBTypes) -> "Query":
+        return Query(model_class, cls().db_name)
+
+
+class Query:
+    def __init__(self, model_class: DBTypes, db_name: str = ""):
+        self.model_class = model_class
+        self.selection = "*"
+        self.filters = {}
+        self.interval = None
+        self.db_name = db_name
+
+    def _build_query(self):
+        """Build the SQL query based on the filters."""
+        query = f"SELECT {self.selection} FROM {self.model_class.__table_name__} "
+        if self.filters:
+            query += "WHERE "
+        conditions = []
+        params = []
+        for key, value in self.filters.items():
+            conditions.append(f"{key} = ?")
+            params.append(value)
+        if self.interval:
+            conditions.append(f"{self.interval.column} BETWEEN ? AND ?")
+            params.extend([self.interval.start_date, self.interval.end_date])
+        query += " AND ".join(conditions) + ";"
+        return query, tuple(params)
+
+    def filter_by(self, **kwargs):
+        """Filter the query based on the provided keyword arguments."""
+        for key, value in kwargs.items():
+            if key in [f.name for f in fields(self.model_class)]:
+                self.filters[key] = value
+            else:
+                raise AttributeError(f"{self.model_class.__name__} has no field {key}")
+        return self
+
+    def between(self, start_date: datetime.datetime, end_date: datetime.datetime, time_column: str = "date"):
+        """Filter the query to include only records between the specified dates."""
+        if time_column not in [f.name for f in fields(self.model_class)]:
+            raise AttributeError(f"{self.model_class.__name__} has no field {time_column}")
+
+        str_format = "%Y-%m-%dT%H:%M:%SZ"
+        start_date = start_date.strftime(str_format)
+        end_date = end_date.strftime(str_format)
+        if start_date > end_date:
+            raise ValueError("Start date must be before end date")
+
+        self.interval = IntervalSelection(time_column, start_date, end_date)
+        return self
+
+    def select(self, *args):
+        """Select specific columns from the query."""
+        self.selection = ", ".join(args)
+        return self
+
+    def all(self) -> list[DBTypes]:
+        """Fetch all results from the query."""
+        query, params = self._build_query()
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            cursor.execute(query, params)
+        result = cursor.fetchall()
+        return [self.model_class(*row) for row in result]
+
+    def one(self) -> DBTypes | None:
+        query, params = self._build_query()
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            cursor.execute(query, params)
+        result = cursor.fetchone()
+        return self.model_class(*result)
 
 
 def get_user(username: str) -> DBUser | None:
-    """Fetches a user by user_id from the database."""
-    with sqlite3.connect(DB_NAME) as conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM users WHERE username = ?;", (username,))
-        user = cursor.fetchone()
-    if user is None:
-        return None
-    return DBUser(*user)
+    """Fetches a user by username from the database."""
+    with SimpleORM(DB_NAME) as orm:
+        quary = "SELECT * FROM users WHERE username = ?;"
+        user = orm.fetch_one(query=quary, params=(username,), return_type=DBUser)
+    return user
 
 
 def get_user_projects(user_id: str) -> list[DBUserProject]:
     """Fetches projects for a user by user_id from the database."""
-    with sqlite3.connect(DB_NAME) as conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM user_projects WHERE user_id = ?;", (user_id,))
-        user_projects = []
-        for project in cursor.fetchall():
-            row_id, *project = project
-            user_projects.append(DBUserProject(*project))
+    with SimpleORM(DB_NAME) as orm:
+        query = "SELECT * FROM user_projects WHERE user_id = ?;"
+        user_projects = orm.fetch_all(query=query, params=(user_id,), return_type=DBUserProject)
     return user_projects
 
 
 def get_all_projects() -> list[DBProject]:
     """Fetches all projects from the database."""
-    with sqlite3.connect(DB_NAME) as conn:
-        cursor = conn.cursor()
-        cursor.execute("""SELECT
-                        project_id,
-                name,
-                location_city,
-                location_country,
-                latitude,
-                longitude,
-                bidding_zone,
-                installed_capacity,
-                description,
-                number_of_cells,
-                unit_price,
-                completed_date,
-                is_completed
-                  FROM projects;""")
-        projects = cursor.fetchall()
-    if projects is None:
-        return []
-    return [
-        DBProject(
-            project_id=project[0],
-            name=project[1],
-            location_city=project[2],
-            location_country=project[3],
-            latitude=project[4],
-            longitude=project[5],
-            bidding_zone=project[6],
-            installed_capacity=project[7],
-            description=project[8],
-            number_of_cells=project[9],
-            unit_price=project[10],
-            completed_date=project[11],
-            is_completed=project[12],
-        )
-        for project in projects
-    ]
+    with SimpleORM(DB_NAME) as orm:
+        query = "SELECT * FROM projects;"
+        projects = orm.fetch_all(query=query, return_type=DBProject)
+    return projects
 
 
 def get_project(project_id: str) -> DBProject | None:
     """Fetches a project by project_id from the database."""
-    with sqlite3.connect(DB_NAME) as conn:
-        cursor = conn.cursor()
-        cursor.execute(
-            (
-                """SELECT 
-                project_id,
-                name,
-                location_city,
-                location_country,
-                latitude,
-                longitude,
-                bidding_zone,
-                installed_capacity,
-                description,
-                number_of_cells,
-                unit_price,
-                completed_date,
-                is_completed
-                FROM projects WHERE project_id = ?;
-                """
-            ),
-            (project_id,),
-        )
-        project = cursor.fetchone()
-    if project is None:
-        return None
-    return DBProject(
-        project_id=project[0],
-        name=project[1],
-        location_city=project[2],
-        location_country=project[3],
-        latitude=project[4],
-        longitude=project[5],
-        bidding_zone=project[6],
-        installed_capacity=project[7],
-        description=project[8],
-        number_of_cells=project[9],
-        unit_price=project[10],
-        completed_date=project[11],
-        is_completed=project[12],
-    )
+    with SimpleORM(DB_NAME) as orm:
+        query = "SELECT * FROM projects WHERE id = ?;"
+        project = orm.fetch_one(query=query, params=(project_id,), return_type=DBProject)
+    return project
 
 
 def get_project_cells(panel_id: str) -> list[DBCell]:
@@ -200,4 +206,16 @@ def get_power_data_range(project_id: str, start_date: str, end_date: str) -> lis
 
 
 if __name__ == "__main__":
-    get_all_projects()
+    import pprint
+
+    orm = SimpleORM()
+    pprint.pprint(
+        orm.quary(DBPowerDataPoint)
+        .filter_by(project_id="CSP_FR_001")
+        .between(
+            start_date=datetime.datetime(2025, 2, 21, 10),
+            end_date=datetime.datetime(2025, 2, 21, 11),
+            time_column="timestamp",
+        )
+        .all()
+    )
